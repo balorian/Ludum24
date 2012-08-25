d47425d8938da34ee731059c2da3b0b325568d81
Leo Vuori <leo.tapio.vuori@gmail.com>
2012-08-25 19:37:18 +0300

    Ludum

---
+++ Ludum/Ludum/Ludum/Game1.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CarmineEngine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Ludum
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        GameScreen testScreen;
+        Player player;
+
+        TextField field;
+        string text;
+
+        Entity[] ground;
+        Entity slope;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            graphics.PreferredBackBufferWidth = (1024);
+            graphics.PreferredBackBufferHeight = (768);
+            graphics.IsFullScreen = false;
+            graphics.ApplyChanges();
+            Window.Title = "Evolution";
+
+            Engine.Setup(graphics, Content, Window);
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            testScreen = new GameScreen("testScreen");
+            SpriteTemplate.LoadFolder("tiles/templates");
+            SpriteTemplate.LoadFolder("sprites/templates");
+
+            Engine.DefaultFont = Content.Load<SpriteFont>("default_font");
+
+            player = new Player(testScreen, new Rectangle(0, 0, 64, 64), "box");
+            player.moveTo(64, 64);
+            field = new TextField(testScreen, new Rectangle(0, 0, 600, 600));
+            field.Tint = Color.Red;
+            field.DrawShadow = true;
+            player.DrawOrder = 100;
+
+            ground = new Entity[50];
+
+            for (int i = 0; i < 50; i++)
+            {
+                Entity tile = new Entity(testScreen, new Rectangle(0, 0, 64, 64), "tile");
+                tile.moveTo(i*64, 700);
+                ground[i] = tile;
+            }
+
+            slope = new Entity(testScreen, "slope", "slope");
+            slope.moveTo(200, 200);
+            slope.UseGeometry = true;
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Engine.Keyboard.pressed(Key.Escape))
+                this.Exit();
+
+            text = player.Velocity.ToString() + "\r\n" + player.Position.ToString();
+            field.Text = text;
+
+            Engine.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            Engine.Draw(gameTime);
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Ludum/Ludum/Ludum/Player.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CarmineEngine;
+using Microsoft.Xna.Framework;
+
+namespace Ludum
+{
+    class Player : Actor
+    {
+        public float MoveAcceleration = 0.005f;
+        public float JumpImpulse = 1f;
+        public Vector2 MaxSpeed = new Vector2(0.6f , 1f);
+
+        public Player(GameScreen screen, Rectangle boundingBox, string sprite) : base(screen, boundingBox, sprite) 
+        {
+            Acceleration.Y = World.Gravity;
+        }
+
+        public override void update()
+        {
+            Ray r = new Ray(new Vector3(1, 0, 0), new Vector3(1, 0, 0));
+            if (Engine.Keyboard.pressed(Key.W))
+                Velocity += JumpImpulse * -Vector2.UnitY;
+
+            if (Engine.Keyboard.down(Key.A))
+            {
+                Acceleration.X = -MoveAcceleration;
+            }
+            if (Engine.Keyboard.down(Key.D))
+            {
+                Acceleration.X = MoveAcceleration;
+            }
+
+            if (Engine.Keyboard.released(Key.D))
+            {
+                Acceleration.X = 0;
+            }
+            if (Engine.Keyboard.released(Key.A))
+            {
+                Acceleration.X = 0;
+            }
+
+            Velocity = Vector2.Clamp(Velocity, -MaxSpeed, MaxSpeed);
+
+            base.update();
+        }
+
+        public override void draw()
+        {
+            base.draw();
+        }
+
+    }
+}
