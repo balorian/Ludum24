8b2a2d3899c246b0652733dadcfd82026a07b53c
Mika Vuori <balorian@gmail.com>
2012-08-25 22:23:16 +0300

    Work on map loading

---
+++ Ludum/CarmineEngine/Level/MapData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CarmineEngine.Level
+{
+    public class MapData
+    {
+        public string name;
+        public string mapImage;
+        public string levelTemplate;
+        public List<LevelDoorData> exitPoints;
+
+    }
+}
---
+++ Ludum/Ludum/Ludum/Level/Tile.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CarmineEngine;
+using Microsoft.Xna.Framework;
+
+namespace Ludum.Level
+{
+    public enum TileType { none, water, block, slope45, block_bg, slope18_left, slope18_right }
+
+    class Tile
+    {
+        public Vector2 Position { get { return position; } set { position = value; TileSprite.Position = value; } }
+        public TileType TileType { get { return type; } }
+        public Sprite TileSprite;
+
+        TileType type;
+        Vector2 position;
+
+        public Tile(GameScreen screen, TileType type, Vector2 position, String template)
+        {
+            this.type = type;
+            TileSprite = new Sprite(screen, template);
+            TileSprite.OverrideDraw = true;
+            TileSprite.OverrideUpdate = true;
+            Position = position;
+        }
+
+        public virtual void draw()
+        {
+            TileSprite.draw();
+        }
+
+        internal void updateTile(TileType[,] n)
+        {
+            switch (type)
+            {
+                case TileType.block:
+                    TileSprite.setFrame(19);
+                    break;
+                case TileType.slope45:
+                    if (n[1, 2] == TileType.none)
+                    { // Below is clear
+                        if (n[0, 1] == TileType.none)
+                        { // Left is clear
+                            TileSprite.setFrame(8);
+                        }
+                        else
+                        {
+                            TileSprite.setFrame(9);
+                        }
+                    }
+                    else
+                    { // Above is clear
+                        if (n[0, 1] == TileType.none)
+                        { // Left is clear
+                            TileSprite.setFrame(3);
+                        }
+                        else
+                        {
+                            TileSprite.setFrame(4);
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
